3cd07166bc8e26afa8f22b542649133e85dfbd1a
ohvash <vascelli.alessandro@gmail.com>
2023-08-09 16:45:08 +0200

    test

--- PlaceReminder/ViewController.m
+++ PlaceReminder/ViewController.m
@@ -7,7 +7,11 @@
 
 #import "ViewController.h"
 
+#define DEBUGLOG(a) NSLog(@"%s: %@", __FUNCTION__, a)
+
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelCount;
+@property (strong, nonatomic) NSNumber *count;
 
 @end
 
@@ -16,6 +20,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.labelCount.text = @"Count:";
+}
+
+-(void)setCount:(NSNumber *)count{
+    _count = count;
+    self.labelCount.text = [NSString stringWithFormat:@"Count: %d", self.count.intValue];
+}
+
+- (IBAction)buttonPressed:(id)sender {
+    self.count = @(self.count.intValue+1);
 }
 
 
